8ab8748b0088fcdc73a0190129ea17a5c94de621
zachbrice <zbrice@banyanlabs.io>
2021-08-05 12:39:44 -0700

    link to firebase

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./signUp";
+import { AuthProvider } from "./Auth";
+import PrivateRoute from "./privateRoute";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
